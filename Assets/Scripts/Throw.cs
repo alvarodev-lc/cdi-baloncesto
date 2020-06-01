ab143ee881f1fd1b7e363514a4925da0ec4079af
javierburgosv <javier.burgosv@alumnos.upm.es>
2020-06-01 13:50:29 +0200

    Throws

---
+++ Assets/Scripts/Throw.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour {
+
+	public GameObject ballPrefab;
+	
+	// Update is called once per frame
+	void Update () {
+
+		bool cKey = Input.GetKeyDown (KeyCode.C);
+		bool spaceKey = Input.GetKeyDown (KeyCode.Space);
+
+		if (cKey || spaceKey){
+
+			GameObject ball = ball = Instantiate(ballPrefab, transform.position, transform.rotation);
+			Vector3 force = new Vector3();
+
+			if (cKey) {
+				force = new Vector3(0f, 13.5f, -10f);
+			}
+
+			if (spaceKey) {
+				force = new Vector3(Random.Range(-1.5f,1.5f), Random.Range(13.5f, 14f), Random.Range(-10f, -11.5f));
+			}
+
+			ball.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+			Destroy(ball, 10f);
+		}
+		
+	}
+}
