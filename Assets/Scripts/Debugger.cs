17834afc06189d001f6205af749cdab9d4b3d915
alvaro.lopez.cruz <alvaro.lopez.cruz@alumnos.upm.es>
2020-06-04 17:29:24 +0200

    Added vuforia assests and compatibility

---
+++ Assets/Scripts/Debugger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debugger : MonoBehaviour {
+
+	private float width;
+	private float heigth;
+
+	// Use this for initialization
+	void Start () {
+		heigth = 2f * gameObject.GetComponent<Camera>().orthographicSize;
+		width = gameObject.GetComponent<Camera>().aspect * heigth;
+		Debug.Log(width);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
